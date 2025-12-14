d7eb31dfa09df2e79daa0e9f97395ba58db44b51
Blakef350211 <florasteven8@gmail.com>
2025-12-13 20:25:48 -0600

    Add documentation comments to Person class
    Added class-level documentation for the Person class.

--- Person.java
+++ Person.java
@@ -1,3 +1,12 @@
+/*
+ * Name: Steven Flora
+ * Course: [Advanced Java / CS 2463 TW01F]
+ * Project: Person GUI Final Project
+ * File: Person.java
+ * Base class representing a person. Stores common attributes
+ * such as first name, last name, and date of birth. This class
+ * serves as the parent class for RegisteredPerson and OCCCPerson.
+ */
 import java.io.Serializable;
 import java.util.Date;
 
@@ -8,3 +17,4 @@ public class Person implements Serializable
 
     public String toString() { return firstName + " " + lastName; }
 }
+
