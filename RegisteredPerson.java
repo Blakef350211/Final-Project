c18cdb2bf3dbbf77925e84f8bebf0f8609177d33
Blakef350211 <florasteven8@gmail.com>
2025-12-13 20:33:23 -0600

    Add documentation comments to RegisteredPerson class
    Added class-level documentation for RegisteredPerson.

--- RegisteredPerson.java
+++ RegisteredPerson.java
@@ -1,6 +1,16 @@
+/*
+ * Name: Steven Flora
+ * Course: [Advanced Java / CS 2463 TW01F]
+ * Project: Person GUI Final Project
+ * File: RegisteredPerson.java
+ * Represents a registered person. Extends the Person class by
+ * adding a government-issued identification number. This class
+ * serves as the parent class for OCCCPerson.
+ */
 public class RegisteredPerson extends Person
  {
     String govId;
 
     public String toString() { return super.toString() + " (Registered)"; }
+
 }
