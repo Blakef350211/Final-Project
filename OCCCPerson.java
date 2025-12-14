5554760c3c76a9d1e796a761be1b363322b58d62
Blakef350211 <florasteven8@gmail.com>
2025-12-13 20:22:03 -0600

    Add documentation comments to OCCCPerson class
    Added class documentation for OCCCPerson.

--- OCCCPerson.java
+++ OCCCPerson.java
@@ -1,6 +1,17 @@
+/*
+ * Name: Steven Flora
+ * Course: [Advanced Java / CS 2463 TW01F]
+ * Project: Person GUI Final Project
+ * File: OCCCPerson.java
+ * Represents an OCCC person. Extends RegisteredPerson by adding
+ * OCCC-specific information such as student or employee ID.
+ * This class is used by the GUI to create and manage OCCCPerson
+ * objects within the application.
+ */
 public class OCCCPerson extends RegisteredPerson
 {
     String occcId;
 
     public String toString() { return super.toString() + " (OCCC)"; }
+
 }
